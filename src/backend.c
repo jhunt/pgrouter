0e886457c7735746249d189034e210eb0109999c
James Hunt <james@niftylogic.com>
2016-04-16 14:27:48 +0000

    Fix backend weighting selection algorithm
    Indexes in the weights array need to match position-for-position those
    in the c->backends array.  Therefore we artificially set weights for
    non-viable backends to 0, causing the cumulative to stay at that of the
    previous (viable) backend.

--- src/backend.c
+++ src/backend.c
@@ -12,8 +12,7 @@ static int negative(int x)
 int pgr_pick_any(CONTEXT *c)
 {
 	int rc, i;
-	int n;        /* number of viable backends (length of weights[]) */
-	int *weights; /* cumulative weights */
+	int *weights;
 	int cumulative;
 
 	rc = rdlock(&c->lock, "context", 0);
@@ -21,7 +20,6 @@ int pgr_pick_any(CONTEXT *c)
 		return negative(rc);
 	}
 
-	/* go ahead and provision weights as if all backends are viable */
 	weights = calloc(c->num_backends, sizeof(int));
 	if (!weights) {
 		unlock(&c->lock, "context", 0);
@@ -29,7 +27,6 @@ int pgr_pick_any(CONTEXT *c)
 		pgr_abort(ABORT_MEMFAIL);
 	}
 
-	n = 0;
 	cumulative = 0;
 	for (i = 0; i < c->num_backends; i++) {
 		rc = rdlock(&c->backends[i].lock, "backend", i);
@@ -43,10 +40,10 @@ int pgr_pick_any(CONTEXT *c)
 		    c->backends[i].health.lag < c->backends[i].health.threshold) {
 
 			cumulative += c->backends[i].weight;
-			weights[n++] = c->backends[i].weight;
+			weights[i] = cumulative;
 		}
 
-		rc = unlock(&c->lock, "backend", i);
+		rc = unlock(&c->backends[i].lock, "backend", i);
 		if (rc != 0) {
 			unlock(&c->lock, "context", 0);
 			return negative(rc);
@@ -58,21 +55,26 @@ int pgr_pick_any(CONTEXT *c)
 		return negative(rc);
 	}
 
-	if (n == 0) {
+	if (cumulative == 0) {
 		pgr_logf(stderr, LOG_ERR, "[backend] no backends are viable!!");
 		return -1;
 	}
 
 	int r = pgr_rand(0, cumulative);
-	for (i = 0; i < n; i++) {
-		if (weights[i] <= r) {
+	pgr_debugf("picking backend using random value %d from (%d,%d)",
+			r, 0, cumulative);
+	for (i = 0; i < c->num_backends; i++) {
+		pgr_debugf("checking backend %d (cumulative weight %d) against %d",
+			i, weights[i], r);
+		if (r <= weights[i]) {
+			pgr_logf(stderr, LOG_INFO, "[backend] using backend %d", i);
 			return i;
 		}
 	}
 
 	/* otherwise, return the first backend, because ... why not */
 	pgr_logf(stderr, LOG_ERR, "unable to pick a random backend from our set of %d (rand [0,%d]) -- returning backend 0 by default",
-			n, cumulative);
+			c->num_backends, cumulative);
 	return 0;
 }
 
